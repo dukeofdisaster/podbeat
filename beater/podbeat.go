a6cf80ac0ddc37ff787929ca956276ec64c705af
boxuser <boxuser@localhost>
2022-11-09 03:45:49 +0000

    add validation helpers and breakout Checkpoint to it's own type

--- beater/podbeat.go
+++ beater/podbeat.go
@@ -1,7 +1,18 @@
 package beater
 
+/*
+TODO:
+	- add params to build url
+	- flesh out checkpoint logic
+		- if first run / checkpoint file does not exist ?
+			- start with Now() and offset
+*/
+
 import (
+	"errors"
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/elastic/beats/v7/libbeat/beat"
@@ -9,7 +20,52 @@ import (
 	"github.com/elastic/beats/v7/libbeat/logp"
 
 	"github.com/dukeofdisaster/podbeat/config"
+	"github.com/gorilla/websocket"
 )
+
+func checkpointExists(c config.Config) (*bool, error) {
+	var f = false
+	var t = true
+	_, err := os.Stat(c.CheckPoint.Path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return &f, nil
+		}
+		return nil, err
+	}
+	return &t, nil
+}
+
+// pod docs only specify 0400 -> 0800 but let's assume the api is sane and accepts others? if not.. womp womp
+func isValidTimezone(s string) bool {
+	switch s {
+	case "0000":
+		return true
+	case "0100":
+		return true
+	case "0200":
+		return true
+	case "0300":
+		return true
+	case "0400":
+		return true
+	case "0500":
+		return true
+	case "0600":
+		return true
+	case "0700":
+		return true
+	case "0800":
+		return true
+	}
+	return false
+}
+
+// cap this at something sane
+func isValidOffset(ago int64) bool {
+	// cap at 2 days worth of minutes... I can't remember if there was a max sinceTime in POD api
+	return (ago >= 0) && (ago < 2880)
+}
 
 // podbeat configuration.
 type podbeat struct {
@@ -34,6 +90,7 @@ func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
 
 // Run starts podbeat.
 func (bt *podbeat) Run(b *beat.Beat) error {
+	//logp.Info("podbeat is running! Hit CTRL-C to stop it.")
 	logp.Info("podbeat is running! Hit CTRL-C to stop it.")
 
 	var err error
@@ -41,26 +98,59 @@ func (bt *podbeat) Run(b *beat.Beat) error {
 	if err != nil {
 		return err
 	}
+	// validate checkpoint path; if exists, try load time, report any errors, else, try create new, report errors
+	checkpoint_ok, err := checkpointExists(bt.config)
+	if err != nil {
+		logp.Error(err)
+		return err
+	}
+	if !*checkpoint_ok {
+		logp.Info("checkpoint path does not exist, first checkpoint will be now epoch")
+	}
+	// validate timezione
+	timezone_ok := isValidTimezone(bt.config.Timezone)
+	if !timezone_ok {
+		logp.Warn("got invalid timezone - may see unexpected results")
+	}
+	// validate offset
+	offset_ok := isValidOffset(bt.config.CheckPoint.Offset)
+	if !offset_ok {
+		logp.Warn("expect the offset to be >= 0 and <= 2880")
+		return fmt.Errorf("expect the offset to be >=0 and <= 2880")
+	}
 
-	ticker := time.NewTicker(bt.config.Period)
-	counter := 1
+	conn, _, err := websocket.DefaultDialer.Dial(bt.config.Endpoint, nil)
+	//ticker := time.NewTicker(1 * time.Second)
+	if err != nil {
+		logp.Error(err)
+		return err
+	}
+	defer conn.Close()
 	for {
-		select {
-		case <-bt.done:
-			return nil
-		case <-ticker.C:
+		// jank issue? when this select statement is present we have no event reads
+		/*
+			select {
+			case <-bt.done:
+				return nil
+			case <-ticker.C:
+			}
+		*/
+		messageType, message, err := conn.ReadMessage()
+		//_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			//logp.Error(err)
+			return err
 		}
-
+		log.Println("message type: ", messageType)
 		event := beat.Event{
 			Timestamp: time.Now(),
 			Fields: common.MapStr{
 				"type":    b.Info.Name,
-				"counter": counter,
+				"message": string(message),
 			},
 		}
 		bt.client.Publish(event)
-		logp.Info("Event sent")
-		counter++
 	}
 }
 
--- config/config.go
+++ config/config.go
@@ -3,12 +3,55 @@
 
 package config
 
-import "time"
-
 type Config struct {
-	Period time.Duration `config:"period"`
+	ApiKey string `config:"apikey"`
+	// this string will also appear in the Bearer token generated for POD access
+	CustomerID string `config:"customerid"`
+	// path to a sqlite db for tracking seen guids to ensure no duplicate events
+	// TODO:
+	//	- choose config opts for max # of guids before we roll the db or table; i.e. new_events | old_events
+	Database struct {
+		Path string `config:"path"`
+	}
+	// Write a periodic check point to disk so the utility can ensure overlap; i.e. no missed events
+	CheckPoint CheckpointType `config:"checkpoint"`
+	Endpoint   string         `config:"endpoint"`
+	// can be one of message|maillog
+	MessageType string `config:"messagetype"`
+	/*
+		Log      struct {
+			Path string `config:"path"`
+		}
+	*/
+	// this will probably fail to be compat with libbeat? thought output was array... maybe not
+	/*
+		Output struct {
+			File struct {
+				Path     string `config:"path"`
+				Filename string `config:"filename"`
+			}
+		}
+	*/
+	// 2020 POD docs define 0400 -> 0700 as 'correct' timezones, don't recall if utc was ever used?
+	Timezone string `config:"timezone"`
+}
+type CheckpointType struct {
+	// a writeable path
+	Path string `config:"path"`
+	// the period in minutes at which periodic checkpoints are written to disk
+	Interval int64 `config:"interval"`
+	// the offset in minutes  to start the event stream from... i.e. if last ran at 13:30 and Offset is 10, then sinceTime
+	// supplied to the Proofpoint API will be 13:20
+	Offset int64 `config:"offset"`
 }
 
 var DefaultConfig = Config{
-	Period: 1 * time.Second,
+	ApiKey:   "aGVsbG93b3JsZAo",
+	Endpoint: "ws://localhost:8080",
+	CheckPoint: CheckpointType{
+		Path:     "/tmp/podbeat.checkpoint",
+		Interval: 15,
+		Offset:   15,
+	},
+	MessageType: "message",
 }
